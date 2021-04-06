067c32185c6a592fe5696a0d9ed3cb412fbfa182
kevinadhiguna <kevin26798@gmail.com>
2021-04-07 00:34:24 +0700

    refactor: add required functions

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit is like a Promise
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Pass empty lists to TodoAdapter
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        // Add click listeners to Add button
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+
+            // Check if the title is not empty.
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+
+        // Add click listeners to Delete button
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }

2bf784e9e2a4e85e23aa3390787fbe2f664996ea
kevinadhiguna <kevin26798@gmail.com>
2021-04-06 23:34:06 +0700

    refactor: add variables

--- app/src/main/java/com/example/todolist/Todo.kt
+++ app/src/main/java/com/example/todolist/Todo.kt
@@ -1,4 +1,8 @@
 package com.example.todolist
 
-class Todo {
+// data class is a class whose primary purpose is to hold data. Hence, no logic presents here.
+
+data class Todo {
+    val title: String,
+    val isChecked: Boolean = false
 }

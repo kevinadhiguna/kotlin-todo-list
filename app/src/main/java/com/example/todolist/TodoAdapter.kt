7d69ae064f62b52fc8eea1bb4fd19338cfca7e2f
kevinadhiguna <kevin26798@gmail.com>
2021-04-06 23:49:08 +0700

    refactor: complete onCreateViewHolder function

--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.todolist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -11,7 +12,13 @@ class TodoAdapter(
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {

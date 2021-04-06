e27f21331d46209baeecc674b7aaf82bb83b6892
kevinadhiguna <kevin26798@gmail.com>
2021-04-06 23:42:53 +0700

    refactor: add functions

--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.todolist
 
-class TodoAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
 }

684a8bf6655c40838c83cca32c08ae89202691dc
kevinadhiguna <kevin26798@gmail.com>
2021-04-06 23:44:24 +0700

    fix: replace braces with parenthesis

--- app/src/main/java/com/example/todolist/Todo.kt
+++ app/src/main/java/com/example/todolist/Todo.kt
@@ -2,7 +2,7 @@ package com.example.todolist
 
 // data class is a class whose primary purpose is to hold data. Hence, no logic presents here.
 
-data class Todo {
+data class Todo (
     val title: String,
     val isChecked: Boolean = false
-}
+)
